a398f9d5e9e63436811329fe8c28b4c9219375f9
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-06-03 21:44:25 -0500

    [Authentication] - implement session using express-session middleware, finish up google oauth authentication using passport-google-oauth

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 // Load User Model
 require('./src/models/User');
@@ -21,6 +24,26 @@ const authRoutes = require('./src/routes/auth');
 
 app.get('/', (req, res) => {
   res.send('Hello, Worlds!');
+});
+
+//
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middelware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global variables
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Use Routes
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,4 +15,17 @@ router.get(
   }
 );
 
+router.get('/verified', (req, res) => {
+  if (req.user) {
+    console.log(req.user);
+  } else {
+    console.log('Not Auth');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/');
+});
+
 module.exports = router;
