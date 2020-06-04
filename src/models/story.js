fb1127145b67b84c896567c99cffdc535e64b9e8
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-06-04 11:17:09 -0500

    [Stories] - create stories StorySchema

---
+++ src/models/story.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StorySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'public',
+  },
+  allowComments: {
+    type: Boolean,
+    default: true,
+  },
+  comments: [
+    {
+      commentsBody: {
+        type: String,
+        required: true,
+      },
+      commentDate: {
+        type: Date,
+        default: Date.now(),
+      },
+      commentUser: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+mongoose.model('stories', StorySchema, 'stories');
